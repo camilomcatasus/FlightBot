c9fdc1869c29ecbf67f4bb5e8caf2f4f2db33745
unkownfire <unkownfire@hotmail.com>
2021-03-17 15:44:10 -0400

    dot env

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
 import main_functions
+import os
+from dotenv import load_dotenv
 url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/MIA-sky/JFK-sky/anytime"
 
 querystring = {"inboundpartialdate":"anytime"}
-
+SECRET_Key = os.getenv("SECRET_KEY")
 headers = {
-    'x-rapidapi-key': "8b810fb046mshf35c1db2d319878p14dfbbjsnd75f1275f083",
+    'x-rapidapi-key': str(SECRET_KEY),
     'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
     }
 
